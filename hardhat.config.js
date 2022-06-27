aa6d152bb7d8fbbb492d8d37b74afe065a2e8c07
aoooio <morisot.dev@gmail.com>
2022-06-27 11:06:46 +0200

    New config for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require('dotenv').config({path:__dirname+'/.env'})
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +18,33 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "mumbai",
+  networks: {
+    goerli: {
+      url: process.env.INFURA_GOERLI_URI,
+      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: process.env.INFURA_MAINNET_URI,
+      accounts: [`${process.env.MAINNET_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: '0.8.11',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800
+      },
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    showTimeSpent: true,
+  },
+  plugins: ['solidity-coverage']
 };
